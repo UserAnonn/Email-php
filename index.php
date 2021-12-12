619aa55943dd3a3d38f121d09a2f2d9abaaa6b58
user <gastewfes@hotmail.com>
2021-12-12 02:08:11 -0300

    sd

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <form action="enviar.php"method="post">
+    <form action="enviar.php"method="POST">
         <div style="margin-top: 5%;margin-left:10%;margin-right: 10%;">
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Dirección de e-mail:</label>

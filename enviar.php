198a146b3aa9b9165c16eccbf671540150c1e6e4
user <gastewfes@hotmail.com>
2021-12-12 02:07:40 -0300

    multiples destinatarios

--- enviar.php
+++ enviar.php
@@ -1,10 +1,11 @@
 <?php
 $to=$_POST['email'];
-$asunto=$_POST['asunto'];
+$subject=$_POST['asunto'];
 $message=$_POST['msg'];
 
 $headers="MIME-Version:1.0" . "\r\n";
-$headers="Content-type:text/plain;charset=UTF-8" . "\r\n";
-$headers="From: Prueba <test@email.com>" . "\r\n";
-mail($to,$asunto,$message,$headers);//funcion propia de php
+$headers .="Content-type:text/plain;charset=UTF-8" . "\r\n";
+$headers .="From: Prueba <test@email.com>" . "\r\n";
+$headers .="Cc: connieansaldi5@gmail.com" ."\r\n gamaccuse@gmail.com";
+mail($to .", connieansaldi5@gmail.com, gamaccuse@gmail.com",$subject,$message,$headers);//funcion propia de php
 ?>

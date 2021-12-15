5d0be1d82bcd63acaa4c4ac65b98ebc830cf3e16
UserAnonn <86017011+UserAnonn@users.noreply.github.com>
2021-12-15 20:02:29 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -7,5 +7,5 @@ $headers="MIME-Version:1.0" . "\r\n";
 $headers .="Content-type:text/plain;charset=UTF-8" . "\r\n";
 $headers .="From: Prueba <test@email.com>" . "\r\n";
 $headers .="Cc: connieansaldi5@gmail.com" ."\r\n gamaccuse@gmail.com";
-mail($to .", connieansaldi5@gmail.com, gamaccuse@gmail.com",$subject,$message,$headers);//funcion propia de php
+mail($to .", mjhhsaldi5@gmail.com, gkjhse@gmail.com",$subject,$message,$headers);//funcion propia de php
 ?>
